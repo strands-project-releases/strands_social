645da05d8b1f25167ad2f8730aa8c90f05457a84
STRANDS Jenkins <strands@hanheide.net>
2014-11-21 20:11:58 +0000

    Rebase from 'release/hydro/card_image_tweet'

--- scripts/tweet_card.py
+++ scripts/tweet_card.py
@@ -5,26 +5,25 @@ import sys
 from time import sleep
 
 import actionlib
-#import json
+import yaml
 
-#from random import randint
+from random import randint
 #from threading import Timer
 
 import strands_tweets.msg
 import image_branding.msg
-#import card_image_tweet.msg
 
-#import cv2
+
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
-import dynamic_reconfigure.client
-#import nhm.srv
+#from cv_bridge import CvBridge, CvBridgeError
+#import dynamic_reconfigure.client
+
 
 
 class read_and_tweet(object):
 
-    def __init__(self):
+    def __init__(self, filename):
 #        rospy.on_shutdown(self._on_node_shutdown)
 
         self.msg_sub = rospy.Subscriber('/socialCardReader/commands', String, self.command_callback, queue_size=1)
@@ -35,7 +34,10 @@ class read_and_tweet(object):
 
         self.client.wait_for_server()
         self.brandclient.wait_for_server()
-
+        text_file = open(filename, "r")
+        
+        texts = yaml.load(text_file)
+        self.tweets_texts = texts['tweets']['card']
         #self.tw_pub = rospy.Publisher('/card_image_tweet/tweet', card_image_tweet.msg.Tweet)
 
         rospy.loginfo(" ... Init done")
@@ -54,8 +56,9 @@ class read_and_tweet(object):
             tweetgoal = strands_tweets.msg.SendTweetGoal()
             brandgoal = image_branding.msg.ImageBrandingGoal()
        
-            #text = tweets[randint(0, len(tweets)-1)]
-            text = "Look who is here"
+            text = self.tweets_texts[randint(0, len(self.tweets_texts)-1)]
+            
+            #text = "Look who is here"
             print "tweeting %s" %text
     
             brandgoal.photo = msg
@@ -86,6 +89,12 @@ class read_and_tweet(object):
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 2 :
+        print "usage: tweet_card file_texts.yaml"
+	sys.exit(2)
+
+    filename=str(sys.argv[1])
+    
     rospy.init_node('card_image_tweet')
-    ps = read_and_tweet()
+    ps = read_and_tweet(filename)
     rospy.spin()
